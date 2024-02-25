ee6e7eaa94416fe7f80d34485b87017cdc0bb8df
eidde606 <eidde_606@yahoo.com>
2024-02-25 12:16:43 -0500

    email update

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -21,7 +21,7 @@ export const ContactUs = () => {
     setFormdata({ loading: true });
 
     const templateParams = {
-      from_name: formData.email,
+      from_email: formData.email,
       user_name: formData.name,
       to_name: contactConfig.YOUR_EMAIL,
       message: formData.message,
