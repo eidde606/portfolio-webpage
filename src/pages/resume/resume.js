778dc2dd0855c9586e47c923cb12360ddad5d182
eidde606 <eidde_606@yahoo.com>
2026-02-17 21:02:05 -0500

    Updated resume

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Col, Container, Row } from "react-bootstrap";
 import { HelmetProvider } from "react-helmet-async";
 import resumePDF from "../../assets/documents/SoftwareEngineer1.pdf";
-import updateResume from "../../assets/images/updateResume.png";
+import SEresume from "../../assets/images/SEresume.png";
 
 import "./style.css";
 
@@ -15,7 +15,7 @@ function Resume() {
           <Col lg="8">
             <h1 className="display-4 mb-4">Resume</h1>
             <hr className="t_border my-4 ml-0 text-left" />
-            <img src={updateResume} alt="resume" className="img-fluid" />
+            <img src={SEresume} alt="resume" className="img-fluid" />
             <hr className="t_border my-2 ml-0 text-left" />
             <a href={resumePDF} download className="resume-download-link">
               <h6 className="display-12 mb-4" style={{ marginRight: "10px" }}>
